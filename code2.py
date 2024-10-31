be8571f039c5c0d433ef57de555aa75e3f76a83b
mhossain69 <mhossain69@myseneca.ca>
2024-10-31 04:39:37 -0400

    Update code2.py

--- code2.py
+++ code2.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
+
+def firstletter(word):
+    return word[0].lower() in {'b', 's'}
+
+def main():
+    colours = ["blue", "red", "green", "silver", "black"]
+    result = [color for color in colours if firstletter(color)]
+    print(result)
+
+if __name__ == "__main__":
+    main()
 
