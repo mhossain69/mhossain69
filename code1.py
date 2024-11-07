85933053ce79bbddad7aafe27ffbdbc02a8f487e
mhossain69 <mhossain69@myseneca.ca>
2024-11-07 10:14:46 -0500

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
+
 import sys
 
 def read_file(filename: str) -> list:
+    """
+    Open a file, return contents as a list of lines.
+    Return a message if the file is not found.
+    """
     try:
         with open(filename, 'r') as file:
             return file.readlines()
     except FileNotFoundError:
-        return ['File not found!']
+        return ["File not found!"]
 
 if __name__ == "__main__":
+    # Check if the script has been provided with a filename as a command-line argument
     if len(sys.argv) < 2:
         print("Error: no file specified.")
     else:
+        # Get the filename from the command-line arguments
         filename = sys.argv[1]
-        lines = read_file(filename)
-        for line in lines:
-            print(line, end="")
-        print(f"\nNumber of lines: {len(lines)}")
+        # Read the file content
+        content = read_file(filename)
+        # Print each line in the file content
+        for line in content:
+            print(line, end='')
+        # Print the number of lines in the content
+        print(f"\nNumber of lines: {len(content)}")
